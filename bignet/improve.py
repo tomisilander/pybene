dd7c9b7f9a8fab830e022feadf1d20b7d5f20f4f
Tomi Silander <tomi.silander@iki.fi>
2023-01-25 18:25:13 +0100

    enhanced cutting a part of small graph

--- bignet/improve.py
+++ bignet/improve.py
@@ -1,25 +1,42 @@
 import numpy as np
 import networkx as nx
 
-def get_subset(g:nx.Graph, rng:np.random.Generator):
-    MAX_N = 10
+def get_subset(g:nx.Graph, max_nof_nodes:int, rng:np.random.Generator) -> set:
+    """_select a random node and nodes around it_
+
+    Args:
+        g (nx.Graph): _a graph from which a subset will be selected_
+        max_nof_nodes (int): _maximum number of nodes selected_
+        rng (np.random.Generator): _random number generator_
+
+    Returns:
+        _set_: _set of nodes connected in g_
+    """
     n = g.number_of_nodes()
-    if n<=MAX_N:
+    if n<=max_nof_nodes:
         return set(g.nodes)
 
     i = rng.integers(n)
     nset = {i}
-    free_space = MAX_N - len(nset)
+    free_space = max_nof_nodes - len(nset)
     while free_space > 0:
-        bs = list(nx.node_boundary(g,nset))
+        bs = list(nx.node_boundary(g, nset))
         nset |= set(rng.choice(bs, min(free_space, len(bs)), replace=False))
-        free_space = MAX_N - len(nset)
+        free_space = max_nof_nodes - len(nset)
     return nset
 
-# to test getting a subset
+def cut_piece(g: nx.Graph, nof_nodes:int, rng:np.random.Generator) -> set:
+    ug = g.to_undirected()
+    nset = set()
+    while len(nset) < nof_nodes and ug.number_of_nodes() > 0:
+        nodes_left = nof_nodes - len(nset)
+        s = get_subset(ug, nodes_left, rng)
+        nset |= s
+        ug.remove_nodes_from(s)
+    return nx.subgraph(g, nset)
+
 
 rng = np.random.default_rng()
 g = nx.gnp_random_graph(20,0.2, directed=True)
-s = get_subset(g.to_undirected(), rng)
-print(s)
-print(g.edges)
+piece = cut_piece(g, 30, rng)
+print(piece.edges)
